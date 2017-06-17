fcdcffa75ed46f2e77c98ed9526a442710cac9e6
Psihaker4 <psihaker4@gmail.com>
2017-06-17 12:44:10 +0300

    updated sketch

--- Arduino/sketchOne.ino
+++ Arduino/sketchOne.ino
@@ -2,26 +2,21 @@
 
 BLEPeripheral blePeripheral;
 BLEService bleService("aaaa");
-BLEIntCharacteristic bleX("aaa0", BLERead | BLEWrite);
-BLEIntCharacteristic bleY("aaa1", BLERead | BLEWrite);
-
-int leftLed = 9;
-int rightLed = 10;
-int bottomLed = 11;
-int topLed = 12;
-
-int nowLed = 13;
+BLEIntCharacteristic bleX("aaa1", BLERead | BLEWrite);
+BLEIntCharacteristic bleY("aaa0", BLERead | BLEWrite);
 
 bool hasCommand = false;
-bool hasCommandC[] = {false, false};
 
 int delayTime = 1000;
-String dir = "";
+
+int x = 0;
+int y = 0;
+
+int controls[2][3] = {{3, 4, 6}, {7, 8, 5}};
 
 void setup() {
-  Serial.begin(9600);
 
-  for (int i = 9; i < 14; i++) {
+  for (int i = 3; i < 9; i++) {
     pinMode(i, OUTPUT);
   }
 
@@ -45,72 +40,100 @@ void setup() {
 
   blePeripheral.begin();
 
-  for (int i = 9; i < 13; i++) {
-    digitalWrite(i, HIGH);
-    delay(100);
-  }
-
-  delay(500);
-
-  for (int i = 12; i > 8; i--) {
-    digitalWrite(i, LOW);
-    delay(100);
-  }
-
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
 }
 
 void loop() {
+
   if (!hasCommand) {
     digitalWrite(13, HIGH);
     delay(100);
     digitalWrite(13, LOW);
     delay(delayTime);
   }
+
 }
 
 void blePeripheralConnect(BLECentral& central) {
-  Serial.print("Connected to central: ");
-  Serial.println(central.address());
   digitalWrite(13, HIGH);
   delayTime = 100;
 }
 
 void blePeripheralDisonnect(BLECentral& central) {
-  Serial.print("Disconnected from central: ");
-  Serial.println(central.address());
-  for (int i = 9; i < 14; i++) {
-    digitalWrite(i, LOW);
-  }
-
   hasCommand = false;
   delayTime = 1000;
 }
 
 void bleXWritten(BLECentral& central, BLECharacteristic& characteristic) {
-  executeCommand(bleX.value(),leftLed, rightLed, 0);
+  x = bleX.value();
+
+  changeDirection(0);
+
 }
 
 void bleYWritten(BLECentral& central, BLECharacteristic& characteristic) {
-  executeCommand(bleY.value(),topLed, bottomLed, 1);
+  y = bleY.value();
+  changeDirection(1);
 }
 
-void executeCommand(int command, int led1, int led2, int i) {
-  hasCommandC[i] = true;
+//engine number: 0 - left; 1 - right
+//dir: 0 - stop; 1 - forward; 2 - backward
+//power: 0 - off; 255 - on;
 
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
+void launchEngine(int number, int dir, int power) {
 
-  if (command == 1) {
-    digitalWrite(led1, HIGH);
-  } else if (command == 2) {
-    digitalWrite(led2, HIGH);
-  } else {
-    hasCommandC[i] = false;
+  if (!dir) power = 0;
+
+  if (power > 255) power = 255;
+  else if (power < 0) power = 0;
+
+  setDirection(number, dir);
+  analogWrite(controls[number][2], power);
+
+}
+
+void stopEngine(int number) {
+  analogWrite(controls[number][2], 0);
+}
+
+void setDirection(int number, int dir) {
+  if (dir) {
+    bool b = dir == 1;
+    digitalWrite(controls[number][0], !b);
+    digitalWrite(controls[number][1], b);
+  }
+}
+
+void changeDirection(int i) {
+
+  int dir0 = x;
+  int dir1 = dir0;
+
+  if (!x) {
+    if (!y) {
+      stopEngine(0);
+      stopEngine(1);
+      return;
+    } else {
+      dir1 = y;
+      dir0 = y == 1 ? 2 : 1;
+    }
   }
 
-  if (hasCommandC[0] || hasCommandC[1]) {
+  launchEngine(0, dir0, 255);
+  launchEngine(1, dir1, 255);
+
+  if (x && y) {
+    stopEngine(y - 1);
+  }
+
+  if (x || y) {
     hasCommand = true;
   } else {
     hasCommand = false;
   }
+
 }
